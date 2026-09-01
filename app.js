e83e1f8dee9dc9bd031934d4fa37ec0535b74d35
aungthuyahan <aungthu.ya659@gmail.com>
2026-09-01 22:12:11 +0300

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
+app.use('/public', express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Welcome to my REST API, Yeah Yahoo!');
 });
