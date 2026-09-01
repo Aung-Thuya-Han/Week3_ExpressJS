af27079034a02b8a6967ccdbb5c2d954131176fe
aungthuyahan <aungthu.ya659@gmail.com>
2026-09-01 22:02:48 +0300

    chore: initialize project with package.json and add express and nodemon dependencies

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my REST API!');
+  res.send('Welcome to my REST API, Yeah Yahoo!');
+});
+
+app.get('/api/v1/cats', (req, res) => {
+  const myData = [
+  {
+    cat_id: 1,
+    name: "My Cat",
+    birthdate: "1.1.2026",
+    weight: 5,
+    owner: "Aung",
+    image: "https://placekittens.com/200/300",
+  }
+];
+  res.json(myData);
 });
 
 app.listen(port, hostname, () => {
